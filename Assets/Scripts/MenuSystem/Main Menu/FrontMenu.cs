d16bce7491982eea8e8503686aa97beea7a1c057
Jasmine Mounes <73711236+warm-wyvern@users.noreply.github.com>
2022-05-26 15:36:29 -0700

    single/multi use same menu fab

--- Assets/Scripts/MenuSystem/Main Menu/FrontMenu.cs
+++ Assets/Scripts/MenuSystem/Main Menu/FrontMenu.cs
@@ -3,24 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class FrontMenu : BaseMenu 
+public class FrontMenu : BaseMenu
 {
+    [SerializeField] GameObject PlayerMenuPrefab;
 
     public void SinglePlayerButton()
     {
-        menuController.EnableMenu(1);
+        GameObject go = Instantiate(PlayerMenuPrefab, transform.parent.parent);
+        MenuController mc = go.GetComponent<MenuController>();
+
+        // single player menu starts out listening to all input channel
+        mc.RewiredID = 0;
+
+        // center single player menu
+        go.transform.localPosition += Vector3.right * 480;
+
+        DisableFrontMenu();
+        Destroy(gameObject);
     }
 
-    
+
     public void MultiplayerButton()
-    {       
+    {
         // enable the Multiplayer Menu Manager, it will enable individual player Menu Controllers
         GameMain.Instance.MultiplayerMenuManager.enabled = true;
 
+        DisableFrontMenu();
+        Destroy(gameObject);
+    }
+
+    public void DisableFrontMenu()
+    {
         // disable the main menu controller (there is no main menu open to control)
         menuController.enabled = false;
         menuController.gameObject.SetActive(false);
-        
-        Destroy(gameObject);
     }
 }
